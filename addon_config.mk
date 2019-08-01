039fcf48fc744098f77f31f9ff011c1267c46c2e
Adam Hinshaw <adam@bigbrowncupboard.com>
2019-08-01 14:38:32 +1000

    Adding addon_config.mk to supress warnings in Android Studio

---
+++ addon_config.mk
@@ -0,0 +1,16 @@
+meta:
+	ADDON_NAME = ofxQuadWarp
+	ADDON_DESCRIPTION = Quad warp transformations using openGL.
+	ADDON_AUTHOR = julapy
+	ADDON_TAGS = 
+	ADDON_URL = https://github.com/julapy/ofxQuadWarp
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	# ADDON_DEPENDENCIES =
+
+	# include search paths, this will be usually parsed from the file system
+	# but if the addon or addon libraries need special search paths they can be
+	# specified here separated by spaces or one per line using +=
+	
